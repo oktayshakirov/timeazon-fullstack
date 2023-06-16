0d12590cf1212d93797185ee59a2a6ec1c864eae
Oktay Shakirov <oktayshakirov@gmail.com>
2023-06-16 23:35:53 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
