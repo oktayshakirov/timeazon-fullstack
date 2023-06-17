74f4f67dc073d3fabaede0997db779f7285767ae
Oktay Shakirov <oktayshakirov@gmail.com>
2023-06-17 23:18:36 +0200

    update

--- api/src/makeRequest.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const makeRequest = axios.create({
-  baseURL: import.meta.env.VITE_APP_API_URL,
-  headers: {
-    Authorization: "Bearer " + import.meta.env.VITE_APP_API_TOKEN,
-  },
-});
